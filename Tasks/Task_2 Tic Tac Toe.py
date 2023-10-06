be9a48a512ac6ccb3ac17b5e283a44ed75fa2a5a
Sanjay Divate <130745671+SanjayD03@users.noreply.github.com>
2023-10-06 19:27:51 +0530

    Add files via upload

---
+++ Tasks/Task_2 Tic Tac Toe.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tic Tac Toe")
+        self.current_player = "X"
+        self.board = [""] * 9
+        self.buttons = []
+        self.create_board()
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                button = tk.Button(self.root, text="", font=("Arial", 24), width=5, height=2,
+                                   command=lambda i=i, j=j: self.on_click(i, j))
+                button.grid(row=i, column=j)
+                row.append(button)
+            self.buttons.append(row)
+
+
+    def on_click(self, i, j):
+        if self.board[i * 3 + j] == "" and not self.check_winner():
+            self.board[i * 3 + j] = self.current_player
+            self.buttons[i][j].config(text=self.current_player)
+            if self.check_winner():
+                messagebox.showinfo("Game Over", f"Player {self.current_player} wins!")
+                self.reset_game()
+            elif "" not in self.board:
+                messagebox.showinfo("Game Over", "It's a tie!")
+                self.reset_game()
+            else:
+                self.toggle_player()
+
+    def toggle_player(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_winner(self):
+        winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                                (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                                (0, 4, 8), (2, 4, 6)]
+
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != "":
+                return True
+        return False
+
+    def reset_game(self):
+        self.board = [""] * 9
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].config(text="")
+        self.current_player = "X"
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
